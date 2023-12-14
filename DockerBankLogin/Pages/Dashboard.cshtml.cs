1cdbb55c6ee4b890a1671c30ff94ad0b0eaeb347
TavvaBhargavi <baru.tavva@gmail.com>
2023-12-14 12:06:02 +0530

    Add project files.

---
+++ DockerBankLogin/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
+
+
+
+
+namespace DockerBankLogin.Pages
+{
+    public class DashboardModel : PageModel
+    {
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Login");
+        }
+
+    }
+
+
+}
+
---
+++ DockerBankLogin/Pages/Login.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DockerBankLogin.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public IActionResult OnPost()
+        {
+            // Simplified logic for demonstration
+            string username = Request.Form["username"];
+            string password = Request.Form["password"];
+
+            // Check if username and password are valid (example)
+            if (username == "user" && password == "pass")
+            {
+                // Redirect to Dashboard on successful login
+                return RedirectToPage("/Dashboard");
+            }
+
+            // Handle invalid credentials or failed login
+            return Page();
+        }
+    }
+}
+
+    
+
